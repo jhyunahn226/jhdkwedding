fc816f7b65d9da761d7eea2033d1f9a05539774c
JHyun Ahn <jhyunahn226@gmail.com>
2024-05-03 00:06:57 +0900

    default layout

---
+++ lib/constants/enum.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ColorEnum {
+  static const black = Color(0xFF000000);
+  static const white = Color(0xFFFFFFFF);
+  static const grey = Color(0xFF808080);
+  static const darkgrey = Color(0xFF505050);
+  static const lightgrey = Color(0xFFD3D3D3);
+  static const red = Color(0xFFBE2623);
+  static const lightred = Color(0xFFE63946);
+  static const blue = Color(0xFF0D1282);
+  static const buttonblue = Color(0xFF3E6DF6);
+  static const bluegreen = Color(0xFF0E3746);
+  static const yellow = Color(0xFFF8DE22);
+  static const kakaoyellow = Color(0xFFFEE500);
+  static const navyblue = Color(0xFF141E46);
+  static const salmon = Color(0xFFFF6969);
+  static const beige = Color(0xFFEAE8DC);
+  static const background = Color(0xFFEFF2F7);
+}
+
+enum GraphMode { month1, month3, year1, all }
