c777891b3071605edf045bbd2782be0f7420b382
JHyun Ahn <jhyunahn226@gmail.com>
2024-05-06 15:06:27 +0900

    메인페이지 후보 추가, 두번째페이지 초안

--- lib/constants/enum.dart
+++ lib/constants/enum.dart
@@ -11,12 +11,17 @@ class ColorEnum {
   static const blue = Color(0xFF0D1282);
   static const buttonblue = Color(0xFF3E6DF6);
   static const bluegreen = Color(0xFF0E3746);
-  static const yellow = Color(0xFFF8DE22);
+
   static const kakaoyellow = Color(0xFFFEE500);
   static const navyblue = Color(0xFF141E46);
   static const salmon = Color(0xFFFF6969);
   static const beige = Color(0xFFEAE8DC);
   static const background = Color(0xFFEFF2F7);
+
+  static const basic = Color(0xFFF5F7F8);
+  static const yellow = Color(0xFFF4CE14);
+  static const green = Color(0xFF495E57);
+  static const darkgreen = Color(0xFF45474B);
 }
 
 enum GraphMode { month1, month3, year1, all }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:jhdkwedding/constants/enum.dart';
 
@@ -17,26 +15,28 @@ class JHDKWedding extends StatelessWidget {
       title: 'JHDK Wedding',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        useMaterial3: true,
-        fontFamily: 'Pretendard',
-        primaryColor: ColorEnum.white,
-        splashFactory: NoSplash.splashFactory,
-        scaffoldBackgroundColor: ColorEnum.white,
-        dialogBackgroundColor: ColorEnum.white,
-        appBarTheme: const AppBarTheme(
-          scrolledUnderElevation: 0,
-          backgroundColor: ColorEnum.white,
-          foregroundColor: ColorEnum.black,
-        ),
-        dialogTheme: const DialogTheme(
-          surfaceTintColor: Colors.transparent,
-          backgroundColor: ColorEnum.white,
-        ),
-        bottomSheetTheme: const BottomSheetThemeData(
-          backgroundColor: ColorEnum.white,
-          surfaceTintColor: Colors.transparent,
-        ),
-      ),
+          useMaterial3: true,
+          fontFamily: 'Pretendard',
+          primaryColor: ColorEnum.white,
+          splashFactory: NoSplash.splashFactory,
+          scaffoldBackgroundColor: ColorEnum.white,
+          dialogBackgroundColor: ColorEnum.white,
+          appBarTheme: const AppBarTheme(
+            scrolledUnderElevation: 0,
+            backgroundColor: ColorEnum.white,
+            foregroundColor: ColorEnum.black,
+          ),
+          dialogTheme: const DialogTheme(
+            surfaceTintColor: Colors.transparent,
+            backgroundColor: ColorEnum.white,
+          ),
+          bottomSheetTheme: const BottomSheetThemeData(
+            backgroundColor: ColorEnum.white,
+            surfaceTintColor: Colors.transparent,
+          ),
+          scrollbarTheme: const ScrollbarThemeData(
+            thickness: MaterialStatePropertyAll(5),
+          )),
       home: const MainScreen(),
     );
   }
@@ -46,71 +46,35 @@ class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
-  State<MainScreen> createState() => _MyHomePageState();
+  State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MyHomePageState extends State<MainScreen> {
-  final PageController _pageController = PageController(initialPage: 0);
-
+class _MainScreenState extends State<MainScreen> {
   @override
   void dispose() {
-    _pageController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final isWebMobile = kIsWeb &&
-        (defaultTargetPlatform == TargetPlatform.iOS ||
-            defaultTargetPlatform == TargetPlatform.android);
     return Scaffold(
       body: SafeArea(
         bottom: false,
-        child: Listener(
-          onPointerSignal: (pointerSignal) {
-            if (pointerSignal is PointerScrollEvent) {
-              if (pointerSignal.scrollDelta.dy > 0 &&
-                  _pageController.page! < 2) {
-                // 아래로 스크롤
-                _pageController.nextPage(
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.easeIn,
-                );
-              } else if (pointerSignal.scrollDelta.dy < 0 &&
-                  _pageController.page! > 0) {
-                // 위로 스크롤
-                _pageController.previousPage(
-                  duration: const Duration(milliseconds: 300),
-                  curve: Curves.easeIn,
-                );
-              }
-            }
-          },
-          child: PageView(
-            controller: _pageController,
-            scrollDirection: Axis.vertical,
-            physics: isWebMobile
-                ? const PageScrollPhysics()
-                : const NeverScrollableScrollPhysics(),
-            children: <Widget>[
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  Positioned.fill(
-                    child: Image.asset(
-                      'assets/photos/1.jpg',
-                      fit: BoxFit.fitHeight,
-                    ),
-                  ),
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            constraints: const BoxConstraints(
+              maxWidth: 500,
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/photos/1.jpg'),
+                  Image.asset('assets/photos/2.jpg'),
+                  Image.asset('assets/photos/3.jpg'),
                 ],
               ),
-              const Center(
-                child: Text('두번째 페이지'),
-              ),
-              const Center(
-                child: Text('세번째 페이지'),
-              ),
-            ],
+            ),
           ),
         ),
       ),
